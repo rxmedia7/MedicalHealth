cb38849ed39701f8b9387a71005d9bdf74926562
Ruslan Khurramov <rxmedia77@gmail.com>
2024-12-21 14:34:06 +0500

    changes added

--- js/main.js
+++ js/main.js
@@ -77,3 +77,12 @@ chatbutton.addEventListener('pointerdown', () => {
 exit.addEventListener('pointerdown', () => {
     livechat.classList.remove("livechatvisible");
 });
+
+//Thumbs-up
+
+let thumbsup = document.querySelector("bi bi-hand-thumbs-up");
+
+thumbsup.addEventListener("pointerdown", () => {
+    thumbsup.classList.add("thumbsred");
+    console.log("Hello");
+});
